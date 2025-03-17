733fabd060b9eae059d520085f0e3bc7b34ea09b
João Victor <joaovictor.dev.io@gmail.com>
2025-03-17 19:28:03 -0300

    Create exec-09.py

---
+++ exec-09.py
@@ -0,0 +1,7 @@
+salario = float(input("Qual seu salario atual R$ "))
+
+reajuste = 15
+reajuste = salario * (reajuste /100)
+novo_salario = salario + reajuste
+
+print("Novo Salario R$ " , novo_salario)
