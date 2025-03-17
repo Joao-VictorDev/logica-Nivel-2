4b46f8e8b0642359f2f944800087b8a0320e7e39
João Victor <joaovictor.dev.io@gmail.com>
2025-03-17 20:01:55 -0300

    Create exec-11.py

---
+++ exec-11.py
@@ -0,0 +1,8 @@
+# Entrada de dados
+fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+
+# Cálculo da conversão
+celsius = (fahrenheit - 32) / 1.8
+
+# Saída do resultado
+print(f"A temperatura em Celsius é: {celsius:.2f}°C")
